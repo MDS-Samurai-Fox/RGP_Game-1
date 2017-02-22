3adad38b7ce299f55c892f5a3429b7b1bd5c0c9d
Juan Rodriguez <trodz24@gmail.com>
2017-02-22 23:40:27 +1300

    Add join/split mechanic

---
+++ Assets/Scripts/Effects/FaceSideSplitter.cs
@@ -0,0 +1,71 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class FaceSideSplitter : MonoBehaviour {
+	
+	[HeaderAttribute("Tweening variables")]
+	[SerializeField]
+    private Ease easeType = Ease.InOutQuad;
+    [SerializeField]
+    private float duration = 1f;
+
+    [HeaderAttribute("Sides of the face")]
+    public Transform leftSide;
+    public Transform middleSide;
+    public Transform rightSide;
+
+    [HeaderAttribute("Position Vectors")]
+    public Vector3 leftSideSplitPosition;
+    public Vector3 middleSideSplitPosition;
+    public Vector3 rightSideSplitPosition;
+
+    private Vector3 leftSideOriginalPosition;
+    private Vector3 middleSideOriginalPosition;
+    private Vector3 rightSideOriginalPosition;
+
+    private bool areSidesJoined = true;
+
+    // Use this for initialization
+    void Start() {
+
+        leftSideOriginalPosition = leftSide.position;
+        middleSideOriginalPosition = middleSide.position;
+        rightSideOriginalPosition = rightSide.position;
+
+        ToggleJoin();
+		
+		Invoke("EnableGravityFloatingBuoyancy", duration);
+
+    }
+
+    public void ToggleJoin() {
+
+        if (areSidesJoined) {
+			
+			leftSide.DOMove(leftSideSplitPosition, duration).SetEase(easeType);
+			middleSide.DOMove(middleSideSplitPosition, duration).SetEase(easeType);
+			rightSide.DOMove(rightSideSplitPosition, duration).SetEase(easeType);
+
+        } else {
+
+			leftSide.DOMove(leftSideOriginalPosition, duration).SetEase(easeType);
+			middleSide.DOMove(middleSideOriginalPosition, duration).SetEase(easeType);
+			rightSide.DOMove(rightSideOriginalPosition, duration).SetEase(easeType);
+
+        }
+
+        areSidesJoined = !areSidesJoined;
+
+    }
+	
+	void EnableGravityFloatingBuoyancy() {
+		
+		print("Enabling Gravity Floating Buoyancy");
+		
+		foreach (GravityFloat gf in GameObject.FindObjectsOfType<GravityFloat>()) {
+			gf.Float();
+		}
+		
+	}
+
+}
--- Assets/Scripts/Effects/GravityFloat.cs
+++ Assets/Scripts/Effects/GravityFloat.cs
@@ -2,44 +2,39 @@
 using UnityEngine;
 
 public class GravityFloat : MonoBehaviour {
-	
-	[SerializeField]
-	private Ease easeType = Ease.OutQuad;
-	[SerializeField]
-	private float moveValue = 0.25f;
-	[SerializeField]
-	private float duration = 2f;
-	[SerializeField]
-	private float delay = 2f;
-	
+
+    [SerializeField]
+    private Ease easeType = Ease.InOutQuad;
+    [SerializeField]
+    private float moveValue = 0.25f;
+    [SerializeField]
+    private float duration = 2f;
+    [SerializeField]
+    private float delay = 2f;
+
     private bool shouldMoveUpwards = false;
-	private float y;
-
-    // Use this for initialization
-    void Start() {
+    [SerializeField]
+	private float yPosition = 0;
+	
+	void Start() {
 		
-		y = this.transform.position.y;
+		// yPosition = this.transform.position.y;
 		
-		Invoke("Float", delay);
+	}
 
-    }
-
-    void Float() {
+    public void Float() {
 
         if (!shouldMoveUpwards) {
 
-            this.transform.DOMoveY(y - moveValue, duration).SetEase(easeType).SetDelay(delay).OnComplete(Float);
-			
-			
+            this.transform.DOMoveY(yPosition - moveValue, duration).SetEase(easeType).SetDelay(delay).OnComplete(Float);
+
+        } else {
+
+            this.transform.DOMoveY(yPosition + moveValue, duration).SetEase(easeType).SetDelay(delay).OnComplete(Float);
+
         }
-		else {
-			
-			this.transform.DOMoveY(y + moveValue, duration).SetEase(easeType).SetDelay(delay).OnComplete(Float);
-			// shouldMoveUpwards = false;
-			
-		}
-		
-		shouldMoveUpwards = !shouldMoveUpwards;
+
+        shouldMoveUpwards = !shouldMoveUpwards;
 
     }
 
