97bfc31794c4d2b8b817cf593682cca1380f9714
Juan Rodriguez <trodz24@gmail.com>
2017-02-23 16:32:44 +1300

    Tweak tweens, add background music

---
+++ Assets/Scripts/_Effects/Buoyancy.cs
@@ -0,0 +1,34 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class Buoyancy : MonoBehaviour {
+
+    // [SerializeField]
+    private Ease easeType = Ease.InOutQuad;
+    // [SerializeField]
+    private float moveValue = 0.1f;
+    // [SerializeField]
+    private float duration = 2f;
+    // [SerializeField]
+    private float delay = 0.1f;
+
+    private bool shouldFloatUpwards = true;
+    private float yPosition = -0.35f;
+
+    public void Float() {
+
+        if (shouldFloatUpwards) {
+
+            this.transform.DOMoveY(yPosition + moveValue, duration).SetEase(easeType).SetDelay(delay).OnComplete(Float);
+
+        } else {
+
+            this.transform.DOMoveY(yPosition - moveValue, duration).SetEase(easeType).SetDelay(delay).OnComplete(Float);
+
+        }
+
+        shouldFloatUpwards = !shouldFloatUpwards;
+
+    }
+
+}
--- Assets/Scripts/_Effects/FaceSideSplitter.cs
+++ Assets/Scripts/_Effects/FaceSideSplitter.cs
@@ -3,92 +3,95 @@ using UnityEngine;
 
 public class FaceSideSplitter : MonoBehaviour {
 
-    [HeaderAttribute("Tweening variables")]
-    [SerializeField]
-    private Ease easeType = Ease.InOutQuad;
-    [SerializeField]
-    private float duration = 1f;
+	[HeaderAttribute("Tweening variables")]
+	[SerializeField]
+	private Ease easeType = Ease.InOutQuad;
+	[SerializeField]
+	private float duration = 1f;
 
-    [HeaderAttribute("Sides of the face")]
-    public Transform leftSide;
-    public Transform middleSide;
-    public Transform rightSide;
+	[HeaderAttribute("Sides of the face")]
+	public Transform faceParent;
+	public Transform leftSide;
+	public Transform middleSide;
+	public Transform rightSide;
 
-    [HeaderAttribute("Position Vectors")]
-    public Vector3 leftSideSplitPosition;
-    public Vector3 middleSideSplitPosition;
-    public Vector3 rightSideSplitPosition;
+	[HeaderAttribute("Position Vectors")]
+	public Vector3 leftSideSplitPosition;
+	public Vector3 middleSideSplitPosition;
+	public Vector3 rightSideSplitPosition;
 
-    private Vector3 leftSideOriginalPosition;
-    private Vector3 middleSideOriginalPosition;
-    private Vector3 rightSideOriginalPosition;
+	private Vector3 leftSideOriginalPosition;
+	private Vector3 middleSideOriginalPosition;
+	private Vector3 rightSideOriginalPosition;
 
-    private bool areSidesJoined = true;
+	private bool areSidesJoined = true;
 
-    private SoundManager sm;
+	private SoundManager soundManager;
 
-    void Awake() {
+	void Awake() {
 
-        sm = GameObject.FindObjectOfType<SoundManager> ();
+		soundManager = GameObject.FindObjectOfType<SoundManager> ();
 
-    }
+	}
 
-    // Use this for initialization
-    void Start() {
+	// Use this for initialization
+	void Start() {
 
-		duration = sm.GetLength(ClipType.Join);
+		duration = soundManager.GetLength(ClipType.Join);
 		
-        leftSideOriginalPosition = leftSide.position;
-        middleSideOriginalPosition = middleSide.position;
-        rightSideOriginalPosition = rightSide.position;
+		leftSideOriginalPosition = leftSide.localPosition;
+		middleSideOriginalPosition = middleSide.localPosition;
+		rightSideOriginalPosition = rightSide.localPosition;
 		
-        ToggleJoin();
+		faceParent.DOMove(new Vector3(0, -0.35f, 0), duration).SetEase(Ease.OutBack).OnComplete(EnableGravityFloatingBuoyancy);
+		ToggleJoin();
 
-        Invoke("EnableGravityFloatingBuoyancy", duration);
+	}
 
-    }
+	public void ToggleJoin() {
 
-    public void ToggleJoin() {
-
-        if (areSidesJoined) {
+		if (areSidesJoined) {
 			
 			Split();
 
-        } else {
+		} else {
 
 			Join();	
 
-        }
+		}
 
-        areSidesJoined = !areSidesJoined;
+		areSidesJoined = !areSidesJoined;
 
-    }
+	}
 
-    void EnableGravityFloatingBuoyancy() {
+	void EnableGravityFloatingBuoyancy() {
 
-        print("Enabling Gravity Floating Buoyancy");
+		print("Enabling Gravity Floating Buoyancy");
 
-        foreach(GravityFloat gf in GameObject.FindObjectsOfType<GravityFloat> ()) {
-            gf.Float();
-        }
+		foreach(Buoyancy gf in GameObject.FindObjectsOfType<Buoyancy> ()) {
+			gf.Float();
+		}
 
-    }
+	}
 	
 	void Join() {
 		
-		sm.Play(ClipType.Join);
-		leftSide.DOMove(leftSideOriginalPosition, duration).SetEase(easeType);
-		middleSide.DOMove(middleSideOriginalPosition, duration).SetEase(easeType);
-		rightSide.DOMove(rightSideOriginalPosition, duration).SetEase(easeType);
+		duration = soundManager.GetLength(ClipType.Join);
+		soundManager.Play(ClipType.Join);
+		
+		leftSide.DOLocalMove(leftSideOriginalPosition, duration).SetEase(easeType);
+		middleSide.DOLocalMove(middleSideOriginalPosition, duration).SetEase(easeType);
+		rightSide.DOLocalMove(rightSideOriginalPosition, duration).SetEase(easeType);
 		
 	}
 	
 	void Split() {
 		
-		sm.Play(ClipType.Split);
-		leftSide.DOMove(leftSideSplitPosition, duration).SetEase(easeType);
-		middleSide.DOMove(middleSideSplitPosition, duration).SetEase(easeType);
-		rightSide.DOMove(rightSideSplitPosition, duration).SetEase(easeType);
+		duration = soundManager.GetLength(ClipType.Split);
+		soundManager.Play(ClipType.Split);
+		leftSide.DOLocalMove(leftSideSplitPosition, duration).SetEase(easeType);
+		middleSide.DOLocalMove(middleSideSplitPosition, duration).SetEase(easeType);
+		rightSide.DOLocalMove(rightSideSplitPosition, duration).SetEase(easeType);
 		
 	}
 
