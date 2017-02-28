b3f3f7b1563af2558450c94de68c5b953f0f8251
Juan Rodriguez <trodz24@gmail.com>
2017-02-28 20:54:37 +1300

    Tweak MenuManager, minor tweaks in the completed scene, added Credits scene to build hierarchy

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,8 +4,8 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour {
 
     private AudioSource audioSource;
-	
-	private static int sceneToChange;
+
+    private static int sceneToChange;
 
     void Awake() {
 
@@ -13,30 +13,41 @@ public class MenuManager : MonoBehaviour {
 
     }
 
+	/// <summary>
+	/// Loads the scene in an ordered manner
+	/// </summary>
+	/// <param name="_scene">The number of the scene in the hierarchy</param>
     public void LoadScene(int _scene) {
-		
-		audioSource.PlayOneShot(audioSource.clip);
 		
 		sceneToChange = _scene;
 
-        Invoke("ChangeScene", audioSource.clip.length / 2);
+        if (audioSource != null) {
+			
+			audioSource.PlayOneShot(audioSource.clip);
+			Invoke("ChangeScene", audioSource.clip.length / 2);
+			
+		} else {
+			
+			Invoke("ChangeScene", 0);
+			
+		}
 
     }
-	
-	void ChangeScene() {
-		SceneManager.LoadScene(sceneToChange);
-	}
+
+    void ChangeScene() {
+        SceneManager.LoadScene(sceneToChange);
+    }
 
     public void Exit() {
-		
-		audioSource.PlayOneShot(audioSource.clip);
+
+        audioSource.PlayOneShot(audioSource.clip);
 
         Invoke("Quit", audioSource.clip.length - 0.2f);
 
     }
-	
-	void Quit() {
-		Application.Quit();
-	}
+
+    void Quit() {
+        Application.Quit();
+    }
 
 }
