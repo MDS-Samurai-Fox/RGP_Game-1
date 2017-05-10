e3554ba26b949de8aae46fd22ad05ed76ebbae37
MDS\mitchell.cur6577 <mitchell.currie@hotmail.com>
2017-05-10 13:25:19 +1200

    Add wheel for changing the face

---
+++ Assets/Scripts/Wheel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Wheel : MonoBehaviour {
+
+    private GameManager gameManager;
+    Vector3 rotationVector;
+
+    void Start() {
+        rotationVector = new Vector3(0.0f, 0.0f, 4.0f);
+    }
+ 
+    void Awake () {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (!gameManager.canUpdate)
+            return;
+
+        Vector3 newRotation = transform.rotation.eulerAngles + rotationVector;
+
+        transform.DORotate(newRotation, 0.1f);
+
+        if (gameManager.faceChecker.HasMatchedFace() == true)
+        {
+            gameManager.timeManager.StopTimer();
+        }
+    }
+}
