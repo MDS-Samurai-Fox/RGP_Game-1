4a4508637b9981d6c9e713653c818997bd903198
MDS\mitchell.cur6577 <mitchell.currie@hotmail.com>
2017-05-17 14:39:26 +1200

    Add Satellite

---
+++ Assets/Scripts/SatelliteController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SatelliteController : MonoBehaviour {
+
+    private float timer = 0.0f;
+    private bool canMove = false;
+    private FadeIn fadeIn;
+    private float fadeTime;
+    private Vector3 panelRotationVector;
+    private Transform panels;
+    private SpriteRenderer baseRenderer;
+    private int movingLeft = 1;
+
+    // Use this for initialization
+    void Start () {
+        fadeIn = GetComponent<FadeIn>();
+        fadeTime = fadeIn.waitTime + 2.0f;
+        panelRotationVector = new Vector3(0.0f, 0.0f, 4.0f);
+        panels = transform.FindChild("Panels");
+        baseRenderer = transform.FindChild("Base").GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+
+        if (timer > fadeTime && !canMove)
+        {
+            canMove = true;
+            MoveLeft();
+        }
+
+        if (canMove)
+        {
+            Vector3 newRotation = panels.rotation.eulerAngles + (panelRotationVector * movingLeft);
+            panels.DORotate(newRotation, 0.1f);
+        }    
+    }
+
+    private void MoveLeft()
+    {
+        transform.DOMove(new Vector3(-7.0f, 0.0f, 0.0f), 11.0f).OnComplete(MoveRight);
+        baseRenderer.flipX = false;
+        movingLeft = 1;
+        // Debug.Log("Satellite Moving left");
+    }
+
+    private void MoveRight()
+    {
+        transform.DOMove(new Vector3(7.0f, 0.0f, 0.0f), 11.0f).OnComplete(MoveLeft);
+        baseRenderer.flipX = true;
+        movingLeft = -1;
+        // Debug.Log("Satellite Moving right");
+    }
+}
