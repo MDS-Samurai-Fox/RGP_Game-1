0df0a8ee0fb6eb9166215ab9dff489eca18de795
SURFACE-CHARMAI\Charmaine <beurre_noisette@hotmail.com>
2017-05-23 01:08:47 +1200

    Updated colors for the "match me" face

--- Assets/Scripts/FaceCheckerNew.cs
+++ Assets/Scripts/FaceCheckerNew.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 
 public class FaceCheckerNew : MonoBehaviour {
 
@@ -38,10 +39,12 @@ public class FaceCheckerNew : MonoBehaviour {
 
     private GameManager gameManager;
 
-    private Color[] skinColor = new Color[5];
-    private Color[] eyeColor = new Color[5];
-    private Color[] lipColor = new Color[5];
-    private Color[] hairColor = new Color[5];
+    private Color[] skinColorArray = new Color[5];
+    private Color[] eyeColorArray = new Color[5];
+    private Color[] lipColorArray = new Color[5];
+    private Color[] hairColorArray = new Color[5];
+
+    private Color chosenSkincolor, chosenEyecolor, chosenLipcolor, chosenHaircolor;
 
     void Awake()
     {
@@ -53,32 +56,29 @@ public class FaceCheckerNew : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
-        //skinColor[0] = new Color(60.0f / 255.0f, 46.0f / 255.0f, 40.0f / 255.0f);
+        skinColorArray[0] = new Color(60.0f / 255.0f, 46.0f / 255.0f, 40.0f / 255.0f);
+        skinColorArray[1] = new Color(105.0f / 255.0f, 80.0f / 255.0f, 70.0f / 255.0f);
+        skinColorArray[2] = new Color(165.0f / 255.0f, 126.0f / 255.0f, 110.0f / 255.0f);
+        skinColorArray[3] = new Color(240.0f / 255.0f, 184.0f / 255.0f, 160.0f / 255.0f);
+        skinColorArray[4] = new Color(255.0f / 255.0f, 229.0f / 255.0f, 200.0f / 255.0f);
 
+        hairColorArray[0] = new Color(9.0f / 255.0f, 8.0f / 255.0f, 6.0f / 255.0f);
+        hairColorArray[1] = new Color(83.0f / 255.0f, 61.0f / 255.0f, 53.0f / 255.0f);
+        hairColorArray[2] = new Color(233.0f / 255.0f, 206.0f / 255.0f, 168.0f / 255.0f);
+        hairColorArray[3] = new Color(183.0f / 255.0f, 166.0f / 255.0f, 158.0f / 255.0f);
+        hairColorArray[4] = new Color(141.0f / 255.0f, 74.0f / 255.0f, 67.0f / 255.0f);
 
-        //Color skincolor1 = new Color(60.0f / 255.0f, 46.0f / 255.0f, 40.0f / 255.0f);
-        //Color skincolor2 = new Color(105.0f / 255.0f, 80.0f / 255.0f, 70.0f / 255.0f);
-        //Color skincolor3 = new Color(165.0f / 255.0f, 126.0f / 255.0f, 110.0f / 255.0f);
-        //Color skincolor4 = new Color(240.0f / 255.0f, 184.0f / 255.0f, 160.0f / 255.0f);
-        //Color skincolor5 = new Color(255.0f / 255.0f, 229.0f / 255.0f, 200.0f / 255.0f);
+        eyeColorArray[0] = new Color(25.0f / 255.0f, 23.0f / 255.0f, 188.0f / 255.0f);
+        eyeColorArray[1] = new Color(69.0f / 255.0f, 24.0f / 255.0f, 0.0f / 255.0f);
+        eyeColorArray[2] = new Color(119.0f / 255.0f, 101.0f / 255.0f, 54.0f / 255.0f);
+        eyeColorArray[3] = new Color(48.0f / 255.0f, 88.0f / 255.0f, 6.0f / 255.0f);
+        eyeColorArray[4] = new Color(61.0f / 255.0f, 69.0f / 255.0f, 80.0f / 255.0f);
 
-        //Color haircolor1 = new Color(9.0f / 255.0f, 8.0f / 255.0f, 6.0f / 255.0f);
-        //Color haircolor2 = new Color(83.0f / 255.0f, 61.0f / 255.0f, 53.0f / 255.0f);
-        //Color haircolor3 = new Color(233.0f / 255.0f, 206.0f / 255.0f, 168.0f / 255.0f);
-        //Color haircolor4 = new Color(183.0f / 255.0f, 166.0f / 255.0f, 158.0f / 255.0f);
-        //Color haircolor5 = new Color(141.0f / 255.0f, 74.0f / 255.0f, 67.0f / 255.0f);
-
-        //Color eyecolor1 = new Color(25.0f / 255.0f, 23.0f / 255.0f, 188.0f / 255.0f);
-        //Color eyecolor2 = new Color(69.0f / 255.0f, 24.0f / 255.0f, 0.0f / 255.0f);
-        //Color eyecolor3 = new Color(119.0f / 255.0f, 101.0f / 255.0f, 54.0f / 255.0f);
-        //Color eyecolor4 = new Color(48.0f / 255.0f, 88.0f / 255.0f, 6.0f / 255.0f);
-        //Color eyecolor5 = new Color(61.0f / 255.0f, 69.0f / 255.0f, 80.0f / 255.0f);
-
-        //Color lipcolor1 = new Color(222.0f / 255.0f, 15.0f / 255.0f, 13.0f / 255.0f);
-        //Color lipcolor2 = new Color(186.0f / 255.0f, 6.0f / 255.0f, 78.0f / 255.0f);
-        //Color lipcolor3 = new Color(227.0f / 255.0f, 93.0f / 255.0f, 106.0f / 255.0f);
-        //Color lipcolor4 = new Color(153.0f / 255.0f, 88.0f / 255.0f, 106.0f / 255.0f);
-        //Color lipcolor5 = new Color(238.0f / 255.0f, 193.0f / 255.0f, 173.0f / 255.0f);
+        lipColorArray[0] = new Color(222.0f / 255.0f, 15.0f / 255.0f, 13.0f / 255.0f);
+        lipColorArray[1] = new Color(186.0f / 255.0f, 6.0f / 255.0f, 78.0f / 255.0f);
+        lipColorArray[2] = new Color(227.0f / 255.0f, 93.0f / 255.0f, 106.0f / 255.0f);
+        lipColorArray[3] = new Color(153.0f / 255.0f, 88.0f / 255.0f, 106.0f / 255.0f);
+        lipColorArray[4] = new Color(238.0f / 255.0f, 193.0f / 255.0f, 173.0f / 255.0f);
 
         if (randomizeFace)
         {
@@ -95,6 +95,11 @@ public class FaceCheckerNew : MonoBehaviour {
             chosenMustache = Mustache[Random.Range(0, Mustache.Length)];
             chosenBeard = Beard[Random.Range(0, Beard.Length)];
 
+            chosenSkincolor = skinColorArray[Random.Range(0, skinColorArray.Length)];
+            chosenEyecolor = eyeColorArray[Random.Range(0, eyeColorArray.Length)];
+            chosenHaircolor = hairColorArray[Random.Range(0, hairColorArray.Length)];
+            chosenLipcolor = lipColorArray[Random.Range(0, lipColorArray.Length)];
+
         }
         else
         {
@@ -109,6 +114,11 @@ public class FaceCheckerNew : MonoBehaviour {
             chosenMustache = Mustache[1];
             chosenBeard = Beard[1];
 
+            chosenSkincolor = skinColorArray[0];
+            chosenEyecolor = eyeColorArray[0];
+            chosenHaircolor = hairColorArray[0];
+            chosenLipcolor = lipColorArray[0];
+
         }
 
         Initialize();
@@ -120,20 +130,38 @@ public class FaceCheckerNew : MonoBehaviour {
     /// </summary>
     void Initialize()
     {
-        Transform t = gameManager.faceToMatch;
-        Transform d = gameManager.faceToMatch.GetChild(0);
-        SpriteRenderer u = gameManager.faceToMatch.GetChild(0).GetComponent<SpriteRenderer>();
-        gameManager.faceToMatch.GetChild(0).GetComponent<SpriteRenderer>().sprite = chosenFace;
-        gameManager.faceToMatch.GetChild(0).GetChild(0).GetComponent<SpriteRenderer>().sprite = chosenEyes;
-        gameManager.faceToMatch.GetChild(0).GetChild(1).GetComponent<SpriteRenderer>().sprite = chosenEyebrows;
-        gameManager.faceToMatch.GetChild(0).GetChild(2).GetComponent<SpriteRenderer>().sprite = chosenRightEar;
-        gameManager.faceToMatch.GetChild(0).GetChild(3).GetComponent<SpriteRenderer>().sprite = chosenLeftEar;
-        gameManager.faceToMatch.GetChild(0).GetChild(4).GetComponent<SpriteRenderer>().sprite = chosenNose;
-        gameManager.faceToMatch.GetChild(0).GetChild(5).GetComponent<SpriteRenderer>().sprite = chosenMouth;
-        gameManager.faceToMatch.GetChild(0).GetChild(6).GetComponent<SpriteRenderer>().sprite = chosenHair;
-        gameManager.faceToMatch.GetChild(0).GetChild(7).GetComponent<SpriteRenderer>().sprite = chosenMustache;
-        gameManager.faceToMatch.GetChild(0).GetChild(8).GetComponent<SpriteRenderer>().sprite = chosenBeard;
+        SpriteRenderer faceRenderer = gameManager.faceToMatch.GetChild(0).GetComponent<SpriteRenderer>();
+        SpriteRenderer eyeRenderer = gameManager.faceToMatch.GetChild(0).GetChild(0).GetComponent<SpriteRenderer>();
+        SpriteRenderer browRenderer = gameManager.faceToMatch.GetChild(0).GetChild(1).GetComponent<SpriteRenderer>();
+        SpriteRenderer rightEarRenderer = gameManager.faceToMatch.GetChild(0).GetChild(2).GetComponent<SpriteRenderer>();
+        SpriteRenderer leftEarRenderer = gameManager.faceToMatch.GetChild(0).GetChild(3).GetComponent<SpriteRenderer>();
+        SpriteRenderer noseRenderer = gameManager.faceToMatch.GetChild(0).GetChild(4).GetComponent<SpriteRenderer>();
+        SpriteRenderer mouthRenderer = gameManager.faceToMatch.GetChild(0).GetChild(5).GetComponent<SpriteRenderer>();
+        SpriteRenderer hairRenderer = gameManager.faceToMatch.GetChild(0).GetChild(6).GetComponent<SpriteRenderer>();
+        SpriteRenderer mustacheRenderer = gameManager.faceToMatch.GetChild(0).GetChild(7).GetComponent<SpriteRenderer>();
+        SpriteRenderer beardRenderer = gameManager.faceToMatch.GetChild(0).GetChild(8).GetComponent<SpriteRenderer>();
 
+        faceRenderer.sprite = chosenFace;
+        eyeRenderer.sprite = chosenEyes;
+        browRenderer.sprite = chosenEyebrows;
+        rightEarRenderer.sprite = chosenRightEar;
+        leftEarRenderer.sprite = chosenLeftEar;
+        noseRenderer.sprite = chosenNose;
+        mouthRenderer.sprite = chosenMouth;
+        hairRenderer.sprite = chosenHair;
+        mustacheRenderer.sprite = chosenMustache;
+        beardRenderer.sprite = chosenBeard;
+
+        faceRenderer.DOColor(chosenSkincolor, 1.0f).SetEase(Ease.InSine);
+        rightEarRenderer.DOColor(chosenSkincolor, 1.0f).SetEase(Ease.InSine);
+        leftEarRenderer.DOColor(chosenSkincolor, 1.0f).SetEase(Ease.InSine);
+        noseRenderer.DOColor(chosenSkincolor, 1.0f).SetEase(Ease.InSine);
+        eyeRenderer.DOColor(chosenEyecolor, 1.0f).SetEase(Ease.InSine);
+        mouthRenderer.DOColor(chosenLipcolor, 1.0f).SetEase(Ease.InSine);
+        browRenderer.DOColor(chosenHaircolor, 1.0f).SetEase(Ease.InSine);
+        hairRenderer.DOColor(chosenHaircolor, 1.0f).SetEase(Ease.InSine);
+        mustacheRenderer.DOColor(chosenHaircolor, 1.0f).SetEase(Ease.InSine);
+        beardRenderer.DOColor(chosenHaircolor, 1.0f).SetEase(Ease.InSine);
     }
 
     /// <summary>
@@ -153,7 +181,11 @@ public class FaceCheckerNew : MonoBehaviour {
             gameManager.faceParent.GetChild(0).GetChild(5).GetComponent<SpriteRenderer>().sprite == chosenMouth &&
             gameManager.faceParent.GetChild(0).GetChild(6).GetComponent<SpriteRenderer>().sprite == chosenHair &&
             gameManager.faceParent.GetChild(0).GetChild(7).GetComponent<SpriteRenderer>().sprite == chosenMustache &&
-            gameManager.faceParent.GetChild(0).GetChild(8).GetComponent<SpriteRenderer>().sprite == chosenBeard
+            gameManager.faceParent.GetChild(0).GetChild(8).GetComponent<SpriteRenderer>().sprite == chosenBeard &&
+            gameManager.faceParent.GetChild(0).GetComponent<SpriteRenderer>().color == chosenSkincolor &&
+            gameManager.faceParent.GetChild(0).GetChild(0).GetComponent<SpriteRenderer>().color == chosenEyecolor &&
+            gameManager.faceParent.GetChild(0).GetChild(5).GetComponent<SpriteRenderer>().color == chosenLipcolor &&
+            gameManager.faceParent.GetChild(0).GetChild(6).GetComponent<SpriteRenderer>().color == chosenHaircolor
         );
 
     }
