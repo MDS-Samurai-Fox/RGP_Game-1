aa7bafcd00ecfa216909d2d9145687c270ed6923
Juan Rodríguez <timrodz@icloud.com>
2018-05-23 14:59:16 +1200

    Add option to share a tweet

---
+++ Assets/Scripts/TwitterHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwitterHandler : MonoBehaviour
+{
+	public void OpenTwitterURL ()
+	{
+		string twitterAddress = "http://twitter.com/intent/tweet";
+
+		string message = "Get this awesome game now!";
+
+		string descriptionParameter = "Space Face";
+
+		string appStoreLink = "https://play.google.com/store/apps/details?id=com.growlgamesstudio.pizZapMania";
+
+		Application.OpenURL (twitterAddress + "?text=" + WWW.EscapeURL (string.Format ("{0} by {1}. {2}", message, descriptionParameter, appStoreLink)));
+	}
+}
